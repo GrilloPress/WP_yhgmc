a3627b4ffcdbcf3f7935a9097078193a6f7403e4
Andrew Duckworth <grillopress@gmail.com>
2016-08-21 21:59:15 +0000

    add featurette styles

--- front-page.php
+++ front-page.php
@@ -59,6 +59,50 @@ get_header(); ?>
   </div>
 </section>
 
+<?php
+$featurettes = CFS()->get( 'featurette_sections' );
+foreach ( $featurettes as $featurette ) { ?>
+
+
+  <section class="page-featurette-section">
+    <div class="container">
+      
+      <div class="row">
+        <div class="col-md-offset-2 col-md-8">
+          <h2><?php echo $featurette['featurette_header'];?></h2>
+          <hr>
+        </div>
+      </div>
+      
+      <?php // Featurette inner loop start
+      $featurette_inners = $featurette['featurette_inner'];
+      foreach ( $featurette_inners as $featurette_inner ) { ?>
+      
+      <div class="col-md-12">
+        <div class="page-featurette-section-inner">
+          <div class="row">
+            <div class="col-md-12">
+              <h3><?php echo $featurette_inner['featurette_card_header'];?></h3>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-4">
+              <?php $featurette_image_id = $featurette_inner[ 'featurette_card_image' ];
+              echo wp_get_attachment_image( $featurette_image_id, 'large', "", array( "class" => "img-marketing" ) );?>
+            </div>
+            <div class="col-md-8">
+              <?php echo $featurette_inner['featurette_card_body'];?>
+            </div>
+          </div> 
+        </div>
+      </div>     
+      <?php };?>
+      
+    </div>
+  </section>
+<?php };?>
+
 <section id="primary" class="page-service-marketing-container">
   <div class="container">
     <div class="row">
@@ -111,47 +155,6 @@ get_header(); ?>
 
   </div>
 </section>
-
-<?php
-$featurettes = CFS()->get( 'featurette_sections' );
-foreach ( $featurettes as $featurette ) { ?>
-
-
-  <section class="page-featurette-section">
-    <div class="container">
-      
-      <div class="row">
-        <div class="col-md-offset-2 col-md-8">
-          <h2><?php echo $featurette['featurette_header'];?></h2>
-          <hr>
-        </div>
-      </div>
-      
-      <?php $featurette_inners = $featurette['featurette_inner']; //CFS()->get( 'featurette_inner' );
-      foreach ( $featurette_inners as $featurette_inner ) { ?>
-      
-      <div class="row">
-        <div class="col-md-12">
-          <h3><?php echo $featurette_inner['featurette_card_header'];?></h3>
-        </div>
-      </div>
-      
-      <div class="row">
-        <div class="col-md-4">
-          <?php $featurette_image_id = $featurette_inner[ 'featurette_card_image' ];
-          echo wp_get_attachment_image( $featurette_image_id, 'large', "", array( "class" => "" ) );?>
-        </div>
-        <div class="col-md-8">
-          <?php echo $featurette_inner['featurette_card_body'];?>
-        </div>
-      </div>      
-      <?php };?>
-      
-    </div>
-  </section>
-
-<?php };?>
-
 
 <section class="page-marketing-four-columns-container">
   <div class="container">
