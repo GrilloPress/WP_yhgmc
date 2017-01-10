ed04d7d7a5371dce9996e7a0255eda804e918bd0
Andrew Duckworth <grillopress@gmail.com>
2017-01-10 11:39:18 +0000

    update logo to new NHS style

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
             <div class="col-md-12">
               <a class="navbar-brand hidden-xs" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
               <a title="<?php bloginfo('name'); ?>" href="<?php echo home_url(); ?>">
-                <img class="sth-logo pull-right" width="250" src="<?php echo get_template_directory_uri() . "/images/yhgmc_logo.png"; ?>">
+                <img class="sth-logo pull-right" width="250" src="<?php echo get_template_directory_uri() . "/images/YHGMC_web.jpg"; ?>">
               </a>
             </div>
           </div>
