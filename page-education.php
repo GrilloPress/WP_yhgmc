2d7051036408474896a09bc4abb9c391dfa49f87
Andrew Duckworth <grillopress@gmail.com>
2017-08-15 15:35:47 +0000

    add updates

--- page-education.php
+++ page-education.php
@@ -66,61 +66,6 @@ get_header(); ?>
         </main><!-- #main -->
 
     </div><!-- #primary -->
-    
-    <div class="row">
-      
-        <?php 
-        // News post item category
-        
-        // WP_Query arguments
-        $col_args = array (
-          
-          'category_name' => 'education',
-          'posts_per_page' => 3
-          
-        );
-   
-        // the query
-        $cat_query = new WP_Query( $col_args ); ?>
-
-        <?php if ( $cat_query->have_posts() ) : ?>
-
-          <!-- the loop -->
-          <?php while ( $cat_query->have_posts() ) : $cat_query->the_post(); ?>
-          
-        <div class="col-md-4">
-          <div class="page-category-card">
-            <?php if ( has_post_thumbnail() ) :?>
-                <a href="<?php the_permalink() ;?>">
-                  <?php the_post_thumbnail('medium', array('class' => 'img-responsive img-full')); ?>
-                </a>
-              <?php else :?>
-                <a href="<?php the_permalink() ;?>">
-                  <img class="img-responsive" src="<?php echo get_template_directory_uri() . "/images/news.jpg"; ?>" alt="News">
-                </a>
-              <?php endif ;?>
-
-            <div class="category-label">
-              News
-            </div>
-              <?php the_title( sprintf( '<h3 class="category-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
-              <?php the_excerpt(); ?>
-          </div>
-      </div>
-        
-          <?php endwhile; ?>
-          <!-- end of the loop -->
-
-          <?php wp_reset_postdata(); ?>
-        
-        
-        <?php else : ?>
-          <p><?php _e( 'Please write a post...' ); ?></p>
-        <?php endif; ?>
-        
-
-      </div>
-    
 
   </div>
 </section>
